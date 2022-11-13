b56d7ea64bf0878ac800b30b1bbccf39f4167423
sonel <sonweb2001@gmail.com>
2022-11-13 08:33:02 +0700

    FE: fix chip info

--- client/src/components/atoms/HeaderInfo/HeaderInfo.tsx
+++ client/src/components/atoms/HeaderInfo/HeaderInfo.tsx
@@ -3,7 +3,12 @@ import clsx from 'clsx';
 
 import styles from './HeaderInfo.module.scss';
 
+import { ReactComponent as IconHeart } from 'assets/images/icon-heart.svg';
+import { ReactComponent as IconChat } from 'assets/images/icon-chat.svg';
+import { ReactComponent as IconEye } from 'assets/images/icon-eye.svg';
 import ProgressBar from 'components/atoms/ProgressBar/ProgressBar';
+import ChipInfo from 'components/atoms/ChipInfo/ChipInfo';
+import { useAppSelector } from 'redux/store';
 
 interface HeaderInfoProps {
   existHeaderMain?: boolean;
@@ -11,6 +16,14 @@ interface HeaderInfoProps {
 
 const HeaderInfo: React.FC<HeaderInfoProps> = ({ existHeaderMain = false }) => {
   const [isShowHeaderInfo, setIsShowHeaderInfo] = useState<boolean>(false);
+  const { avatarAuthPost, titlePost, viewPost, totalCommentPost } =
+    useAppSelector((state) => ({
+      avatarAuthPost: state.post.post?.authPost.avatar,
+      titlePost: state.post.post?.title,
+      viewPost: state.post.post?.view,
+      totalCommentPost: state.post.post?.totalComment,
+    }));
+
   useEffect(() => {
     const handleScroll = () => {
       setTimeout(() => {
@@ -34,13 +47,15 @@ const HeaderInfo: React.FC<HeaderInfoProps> = ({ existHeaderMain = false }) => {
     >
       <header className={'container-full'}>
         <div className={styles.infoPost}>
-          <img
-            src="https://ncmaz.chisnghiax.com/wp-content/uploads/2021/09/bqe0j0b26rq-1-e1633222495376-150x150.jpg"
-            alt=""
-            width={44}
-            height={44}
-          />
-          <div>title post</div>
+          <img src={avatarAuthPost || ''} alt="" width={44} height={44} />
+          <div className={styles.titlePost}>{titlePost || ''}</div>
+
+          <div className={styles.dataPost}>
+            <ChipInfo icon={<IconEye />} total={viewPost} dark />
+            <ChipInfo icon={<IconHeart />} total={5} dark />
+            <div className={styles.postInfoLine}></div>
+            <ChipInfo icon={<IconChat />} total={totalCommentPost} dark />
+          </div>
         </div>
       </header>
       <ProgressBar />
