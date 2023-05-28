39814d79877864f853378c72e7d99d605813581d
sonle <sonweb2001@gmail.com>
2023-05-28 21:45:20 +0700

    FE: fix condition redirect to home

--- client/src/hooks/useScrollHeader.ts
+++ client/src/hooks/useScrollHeader.ts
@@ -1,12 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export const useHideHeaderScroll = () => {
   const [lastScrollY, setLastScrollY] = useState<number>(0);
   const [isHideHeader, setIsHideHeader] = useState<boolean>(false);
+  const currentId = useRef<any>();
 
   useEffect(() => {
+    if (currentId.current) {
+      clearTimeout(currentId.current);
+    }
+
     const handleScroll = () => {
-      setTimeout(() => {
+      currentId.current = setTimeout(() => {
         setIsHideHeader(window.scrollY > lastScrollY);
         setLastScrollY(window.scrollY);
       }, 150);
@@ -16,6 +21,7 @@ export const useHideHeaderScroll = () => {
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      clearTimeout(currentId.current);
     };
   }, [lastScrollY]);
 
